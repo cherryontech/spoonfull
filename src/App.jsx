de21558289ae161129b84ef7be37ad1f41d7319c
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-10-19 15:04:39 -0400

    placed all of the elements on the page, styled header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddTaskModal from './components/AddTaskModal/AddTaskModal';
+import TasksPage from './pages/TasksPage/TasksPage';
 import "@fontsource/lato";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
      <Routes>
       <Route path="/modal" element={<AddTaskModal />}/>
+      <Route path="/" element={<TasksPage />}/>
      </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/TasksPage/TasksPage.jsx
@@ -0,0 +1,22 @@
+import heroImg from "../../assets/heroImg.png";
+
+const TasksPage = () => {
+    return (
+        <section className="bg-background w-[100vw] h-[100vh] p-4">
+            <div className="w-[100%] flex justify-between items-center border-b border-divider pb-2">
+                <h4 className="text-header4">Tasks</h4>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path d="M10 18H14V16H10V18ZM3 6V8H21V6H3ZM6 13H18V11H6V13Z" fill="#0F0129"/>
+                </svg>  
+            </div>
+            <div className="h-[100%] flex flex-col justify-center items-center">
+                <img src={heroImg} alt="a graphic with a girl standing next to big phone, managing tasks" className="w-[200px]"/>
+                <h5 className="text-subtitle">Start adding your tasks!</h5>
+                <p className="text-caption">Plan your day by adding tasks and allocating your energy among them.</p>
+            </div>
+        </section>
+    )
+}
+
+
+export default TasksPage;
