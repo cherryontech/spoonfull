e1debab3ec92bfa385acbea26de6d525528d753b
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-10-22 13:31:48 -0400

    change the rendering logic on the TaskList page and set a get method for localStorage

--- src/pages/TasksPage/TasksPage.jsx
+++ src/pages/TasksPage/TasksPage.jsx
@@ -7,17 +7,21 @@ import AddTaskModal from "../../components/AddTaskModal/AddTaskModal";
 const TasksPage = () => {
     const [showModal, setShowModal] = useState(false);
     const [taskList, setTaskList] = useState([]);
-
+    
     const openModal = (e) => {
         e.preventDefault();
         setShowModal(true)
     }
 
     useEffect(() => {
-       const data = window.localStorage.getItem('TASK');
-       setTaskList(JSON.parse(data))
+        if (!localStorage["tasks"]) {
+            localStorage["tasks"] = "[]";
+          }
+        
+          let tasks = localStorage["tasks"];
+          tasks = JSON.parse(tasks);
+          setTaskList(tasks)
     }, []);
-
 
     return (
         <section className="bg-background w-[100vw] h-[100vh] p-4">
@@ -27,23 +31,18 @@ const TasksPage = () => {
                     <path d="M10 18H14V16H10V18ZM3 6V8H21V6H3ZM6 13H18V11H6V13Z" fill="#0F0129"/>
                 </svg>  
             </div>
-            <div className={
-                taskList?
-                 "h-[650px] flex flex-col justify-center items-center gap-4 hidden" 
-                 : 
-                 "h-[650px] flex flex-col justify-center items-center gap-4"
-            }>
-                <img src={heroImg} alt="a graphic with a girl standing next to big phone, managing tasks" className="w-[200px]"/>
-                <h5 className="text-subtitle">Start adding your tasks!</h5>
-                <p className="text-caption text-light-text text-center w-[200px]">Plan your day by adding tasks and allocating your energy among them.</p>
-            </div>
-            <div className={
-                !taskList? 
-                "h-[650px] hidden" 
-                : 
-                "h-[650px]"
-            }>
-            </div>
+            {
+                (!taskList[0] || !taskList[0].task)?
+                <div className= "h-[650px] flex flex-col justify-center items-center gap-4">
+                    <img src={heroImg} alt="a graphic with a girl standing next to big phone, managing tasks" className="w-[200px]"/>
+                    <h5 className="text-subtitle">Start adding your tasks!</h5>
+                    <p className="text-caption text-light-text text-center w-[200px]">Plan your day by adding tasks and allocating your energy among them.</p>
+                </div>
+                :
+                <div className="h-[650px]">
+                    <h4>{taskList[0].task}</h4>
+                </div>
+            }
             <button onClick={openModal}>Open Modal</button>
             {showModal && createPortal(
                 <AddTaskModal />,
