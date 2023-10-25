2f6f84d76b13d5d10c6746009bc7cb9a91c4ad3d
TanyaPina <tanyapina15@gmail.com>
2023-10-24 19:27:32 -0700

    added checkbox

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -2,10 +2,12 @@ const TaskCard = () => {
 
     return (
         <div className="w-full p-4">
-            <div className=" bg-navbar-background rounded-2xl p-4 flex flex-col justify-between">
-                <div className="my-2">
-                    <div className="text-gray-900 mb-2">Can coffee make you a better developer?</div>
-                    <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <div className="bg-navbar-background rounded-2xl p-4 flex flex-col justify-between">
+                <div className="my-2 flex gap-2">
+                    <input
+                        className="appearance-none w-4 h-4 border rounded-sm mt-1 shrink-0"
+                        type="checkbox" />
+                    <label className="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.</label>
                 </div>
             </div>
         </div>
