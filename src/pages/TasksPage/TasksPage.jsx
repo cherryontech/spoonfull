95a714ae94c50d9ad763164a9a5e8f0dcc8bb900
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-10-27 18:46:10 -0400

    removed filter icon

--- src/pages/TasksPage/TasksPage.jsx
+++ src/pages/TasksPage/TasksPage.jsx
@@ -35,9 +35,6 @@ const TasksPage = () => {
         <section className="bg-background w-[100vw] h-[100vh] p-4">
             <div className="w-[100%] flex justify-between items-center border-b border-divider pb-2">
                 <h4 className="text-header4">Tasks</h4>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <path d="M10 18H14V16H10V18ZM3 6V8H21V6H3ZM6 13H18V11H6V13Z" fill="#0F0129" />
-                </svg>
             </div>
             {
                 (!taskList[0] || !taskList[0].task) ?
