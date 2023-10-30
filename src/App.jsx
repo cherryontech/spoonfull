7254ea7e18c46a42ca152222c492bde1c4725a8d
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-10-30 19:23:37 -0400

    fixed styling issue with the modal not being wide enought for full desktop screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TasksPage from './pages/TasksPage/TasksPage';
 import Navbar from './components/Navbar/Navbar';
-import WelcomePage from './pages/WelcomePage/WelcomePage';
 import "@fontsource/lato";
 
 function App() {
