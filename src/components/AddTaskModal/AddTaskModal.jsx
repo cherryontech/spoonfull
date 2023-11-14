0dd32971799313cd807ae157c0f475e8464b7707
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-11-14 11:44:23 -0500

    fixed tooltip

--- src/components/AddTaskModal/AddTaskModal.jsx
+++ src/components/AddTaskModal/AddTaskModal.jsx
@@ -2,9 +2,6 @@ import { useState } from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Listbox } from '@headlessui/react'
-import TooltipModal from "../TooltipModal/TooltipModal";
-import { createPortal } from "react-dom";
-
 
 // eslint-disable-next-line react/prop-types
 const AddTaskModal = ({ setShowModal, remainingSpoons, handleTaskAdded }) => {
@@ -38,7 +35,7 @@ const AddTaskModal = ({ setShowModal, remainingSpoons, handleTaskAdded }) => {
         } else {
             setErrorMessage(false)
         }
-        
+
         setTaskName(newTask);
     }
 
@@ -96,9 +93,13 @@ const AddTaskModal = ({ setShowModal, remainingSpoons, handleTaskAdded }) => {
         
     const handleOpenTooltip = (e) => {
         e.preventDefault();
-        setOpenTooltip(true)
+        if(openTooltip === false){
+            setOpenTooltip(true)
+        } else {
+            setOpenTooltip(false)
+        }  
     }
-
+    
     return (
         <section className=" bg-text2 w-[100vw] h-[100vh] flex justify-center items-center fixed top-0" onClick={() => setShowModal(false)}>
             <ToastContainer 
@@ -142,15 +143,30 @@ const AddTaskModal = ({ setShowModal, remainingSpoons, handleTaskAdded }) => {
                     <div className="pb-6 pt-2.5">
                         <div className="flex items-center gap-2 mb-2">
                             <h5 className="text-subtitle">Assign spoons</h5>
-                            <button onClick={handleOpenTooltip}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <path d="M11 17H13V11H11V17ZM12 9C12.2833 9 12.521 8.904 12.713 8.712C12.905 8.52 13.0007 8.28267 13 8C13 7.71667 12.904 7.479 12.712 7.287C12.52 7.095 12.2827 6.99933 12 7C11.7167 7 11.479 7.096 11.287 7.288C11.095 7.48 10.9993 7.71733 11 8C11 8.28333 11.096 8.521 11.288 8.713C11.48 8.905 11.7173 9.00067 12 9ZM12 22C10.6167 22 9.31667 21.7373 8.1 21.212C6.88333 20.6867 5.825 19.9743 4.925 19.075C4.025 18.175 3.31267 17.1167 2.788 15.9C2.26333 14.6833 2.00067 13.3833 2 12C2 10.6167 2.26267 9.31667 2.788 8.1C3.31333 6.88333 4.02567 5.825 4.925 4.925C5.825 4.025 6.88333 3.31267 8.1 2.788C9.31667 2.26333 10.6167 2.00067 12 2C13.3833 2 14.6833 2.26267 15.9 2.788C17.1167 3.31333 18.175 4.02567 19.075 4.925C19.975 5.825 20.6877 6.88333 21.213 8.1C21.7383 9.31667 22.0007 10.6167 22 12C22 13.3833 21.7373 14.6833 21.212 15.9C20.6867 17.1167 19.9743 18.175 19.075 19.075C18.175 19.975 17.1167 20.6877 15.9 21.213C14.6833 21.7383 13.3833 22.0007 12 22Z" fill="black"/>
-                                </svg>
-                            </button>
-                            {openTooltip && createPortal(
-                                <TooltipModal setOpenTooltip={setOpenTooltip}/>,
-                                document.body
-                            )}
+                            <div className="relative flex justify-center items-center">
+                            {openTooltip? 
+                                <div className="bg-text px-2 py-1 rounded w-[168px] absolute z-20 bottom-[36px] left-[0px]" onClick={() => setOpenTooltip(false)}>
+                                    <p className="text-caption text-background">You can assign a number of spoons to indicate the energy this task might take. Ideally everyday you try not to go over 12 spoons.</p>
+                                </div>
+                                :
+                                <div className="hidden bg-text px-2 py-1 rounded w-[168px] absolute z-20 bottom-[36px] left-[0px]" onClick={() => setOpenTooltip(false)}>
+                                    <p className="text-caption text-background">You can assign a number of spoons to indicate the energy this task might take. Ideally everyday you try not to go over 12 spoons.</p>
+                                </div>
+                            } 
+                            {(window.innerWidth < 768)?
+                                <button onClick={handleOpenTooltip}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                        <path d="M11 17H13V11H11V17ZM12 9C12.2833 9 12.521 8.904 12.713 8.712C12.905 8.52 13.0007 8.28267 13 8C13 7.71667 12.904 7.479 12.712 7.287C12.52 7.095 12.2827 6.99933 12 7C11.7167 7 11.479 7.096 11.287 7.288C11.095 7.48 10.9993 7.71733 11 8C11 8.28333 11.096 8.521 11.288 8.713C11.48 8.905 11.7173 9.00067 12 9ZM12 22C10.6167 22 9.31667 21.7373 8.1 21.212C6.88333 20.6867 5.825 19.9743 4.925 19.075C4.025 18.175 3.31267 17.1167 2.788 15.9C2.26333 14.6833 2.00067 13.3833 2 12C2 10.6167 2.26267 9.31667 2.788 8.1C3.31333 6.88333 4.02567 5.825 4.925 4.925C5.825 4.025 6.88333 3.31267 8.1 2.788C9.31667 2.26333 10.6167 2.00067 12 2C13.3833 2 14.6833 2.26267 15.9 2.788C17.1167 3.31333 18.175 4.02567 19.075 4.925C19.975 5.825 20.6877 6.88333 21.213 8.1C21.7383 9.31667 22.0007 10.6167 22 12C22 13.3833 21.7373 14.6833 21.212 15.9C20.6867 17.1167 19.9743 18.175 19.075 19.075C18.175 19.975 17.1167 20.6877 15.9 21.213C14.6833 21.7383 13.3833 22.0007 12 22Z" fill="black"/>
+                                    </svg>
+                                </button>
+                                :
+                                <button onClick={handleOpenTooltip} onMouseEnter={() => setOpenTooltip(true)} onMouseLeave={() => setOpenTooltip(false)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                        <path d="M11 17H13V11H11V17ZM12 9C12.2833 9 12.521 8.904 12.713 8.712C12.905 8.52 13.0007 8.28267 13 8C13 7.71667 12.904 7.479 12.712 7.287C12.52 7.095 12.2827 6.99933 12 7C11.7167 7 11.479 7.096 11.287 7.288C11.095 7.48 10.9993 7.71733 11 8C11 8.28333 11.096 8.521 11.288 8.713C11.48 8.905 11.7173 9.00067 12 9ZM12 22C10.6167 22 9.31667 21.7373 8.1 21.212C6.88333 20.6867 5.825 19.9743 4.925 19.075C4.025 18.175 3.31267 17.1167 2.788 15.9C2.26333 14.6833 2.00067 13.3833 2 12C2 10.6167 2.26267 9.31667 2.788 8.1C3.31333 6.88333 4.02567 5.825 4.925 4.925C5.825 4.025 6.88333 3.31267 8.1 2.788C9.31667 2.26333 10.6167 2.00067 12 2C13.3833 2 14.6833 2.26267 15.9 2.788C17.1167 3.31333 18.175 4.02567 19.075 4.925C19.975 5.825 20.6877 6.88333 21.213 8.1C21.7383 9.31667 22.0007 10.6167 22 12C22 13.3833 21.7373 14.6833 21.212 15.9C20.6867 17.1167 19.9743 18.175 19.075 19.075C18.175 19.975 17.1167 20.6877 15.9 21.213C14.6833 21.7383 13.3833 22.0007 12 22Z" fill="black"/>
+                                    </svg>
+                                </button>
+                            }
+                            </div>
                         </div>
                         <div className="flex gap-2 justify-between items-center">
                             <div className="flex gap-2 p-2 justify-between items-center border-[0.4px] rounded-lg">
