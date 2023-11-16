41f3949e308e1c13ec0a9ec4671e6b88a69228ea
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-11-15 19:56:29 -0500

    changed the tutorial screen according to the design

--- src/components/TutorialFourthScreen/TutorialFourthScreen.jsx
+++ src/components/TutorialFourthScreen/TutorialFourthScreen.jsx
@@ -7,28 +7,54 @@ const TutorialFourthScreen = () => {
     return (
         <section className="center-column gap-6 self-center text-center">
             <h4 className="text-header4">Customize your tasks</h4>
-            <p className="text-body text-text1 w-[256px]">A modal appears for task details when you click “Add Task” as shown below</p>
-            <div>
-                <div className="flex justify-start items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="35" viewBox="0 0 34 35" fill="none">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M18.2508 18.6791L8.60447 19.7921C8.27704 19.8301 7.94528 19.7984 7.63098 19.6991C7.31667 19.5998 7.02693 19.4351 6.78079 19.2158C6.53464 18.9966 6.33766 18.7278 6.20278 18.427C6.0679 18.1262 5.99817 17.8003 5.99817 17.4707C5.99817 17.1411 6.0679 16.8152 6.20278 16.5144C6.33766 16.2136 6.53464 15.9448 6.78079 15.7256C7.02693 15.5063 7.31667 15.3416 7.63098 15.2423C7.94528 15.143 8.27704 15.1113 8.60447 15.1493L18.2508 16.2623C18.7713 14.9343 20.1261 13.4706 22.4998 13.4699C23.936 13.4706 25.2611 13.9019 26.2496 14.585C27.2113 15.2497 27.9997 16.2658 27.9997 17.4707C28.0004 18.6749 27.2113 19.6917 26.2503 20.3557C25.2618 21.0388 23.936 21.4708 22.4998 21.4701C20.1247 21.4708 18.7713 20.0071 18.2508 18.6791Z" fill="#23A1AF"/>
-                    </svg>
-                    <p className="text-body text-text1">Enter Task Name</p>
+            <p className="text-body text-text1 w-[256px]">{`After clicking "Add Task", a pop-up will appear allowing you to enter details for your task`}</p>
+            <div className="flex flex-col justify-start">
+                <div className="flex flex-col justify-start gap-2">
+                    <div className="flex gap-2 justify-start items-center">
+                        <div className="relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M11.9997 2.75184C18.6567 -4.09116 35.3007 7.88334 11.9997 23.2808C-11.3013 7.88484 5.34267 -4.09116 11.9997 2.75184Z" fill="#F7559E"/>
+                            </svg>
+                            <p className="text-white text-caption absolute top-0 left-[30%]">1</p>
+                        </div>
+                        <p className="text-body text-text1">Enter Task Name</p>
+                    </div>
                 </div>
-                <div className="flex justify-start items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="35" viewBox="0 0 34 35" fill="none">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M18.2508 18.6791L8.60447 19.7921C8.27704 19.8301 7.94528 19.7984 7.63098 19.6991C7.31667 19.5998 7.02693 19.4351 6.78079 19.2158C6.53464 18.9966 6.33766 18.7278 6.20278 18.427C6.0679 18.1262 5.99817 17.8003 5.99817 17.4707C5.99817 17.1411 6.0679 16.8152 6.20278 16.5144C6.33766 16.2136 6.53464 15.9448 6.78079 15.7256C7.02693 15.5063 7.31667 15.3416 7.63098 15.2423C7.94528 15.143 8.27704 15.1113 8.60447 15.1493L18.2508 16.2623C18.7713 14.9343 20.1261 13.4706 22.4998 13.4699C23.936 13.4706 25.2611 13.9019 26.2496 14.585C27.2113 15.2497 27.9997 16.2658 27.9997 17.4707C28.0004 18.6749 27.2113 19.6917 26.2503 20.3557C25.2618 21.0388 23.936 21.4708 22.4998 21.4701C20.1247 21.4708 18.7713 20.0071 18.2508 18.6791Z" fill="#23A1AF"/>
-                    </svg>
-                    <p className="text-body text-text1">Enter the number of spoons</p>
+                <div className="flex flex-col justify-start gap-2">
+                    <div className="flex gap-2 justify-start items-center">
+                        <div className="relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M11.9997 2.75184C18.6567 -4.09116 35.3007 7.88334 11.9997 23.2808C-11.3013 7.88484 5.34267 -4.09116 11.9997 2.75184Z" fill="#F7559E"/>
+                            </svg>
+                            <p className="text-white text-caption absolute top-0 left-[30%]">2</p>
+                        </div>
+                        <p className="text-body text-text1">Enter the number of spoons</p>
+                    </div>
                 </div>
-                <div className="flex justify-start items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="35" viewBox="0 0 34 35" fill="none">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M18.2508 18.6791L8.60447 19.7921C8.27704 19.8301 7.94528 19.7984 7.63098 19.6991C7.31667 19.5998 7.02693 19.4351 6.78079 19.2158C6.53464 18.9966 6.33766 18.7278 6.20278 18.427C6.0679 18.1262 5.99817 17.8003 5.99817 17.4707C5.99817 17.1411 6.0679 16.8152 6.20278 16.5144C6.33766 16.2136 6.53464 15.9448 6.78079 15.7256C7.02693 15.5063 7.31667 15.3416 7.63098 15.2423C7.94528 15.143 8.27704 15.1113 8.60447 15.1493L18.2508 16.2623C18.7713 14.9343 20.1261 13.4706 22.4998 13.4699C23.936 13.4706 25.2611 13.9019 26.2496 14.585C27.2113 15.2497 27.9997 16.2658 27.9997 17.4707C28.0004 18.6749 27.2113 19.6917 26.2503 20.3557C25.2618 21.0388 23.936 21.4708 22.4998 21.4701C20.1247 21.4708 18.7713 20.0071 18.2508 18.6791Z" fill="#23A1AF"/>
-                    </svg>
-                    <p className="text-body text-text1">Click on “Add” to finish</p>
+                <div className="center-columnflex flex-col justify-start gap-2">
+                    <div className="flex gap-2 justify-start items-center">
+                        <div className="relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M11.9997 2.75184C18.6567 -4.09116 35.3007 7.88334 11.9997 23.2808C-11.3013 7.88484 5.34267 -4.09116 11.9997 2.75184Z" fill="#F7559E"/>
+                            </svg>
+                            <p className="text-white text-caption absolute top-0 left-[30%]">3</p>
+                        </div>
+                        <p className="text-body text-text1">Select priority from dropdown</p>
+                    </div>
+                </div>
+                <div className="flex flex-col justify-start gap-2">
+                    <div className="flex gap-2 justify-start items-center">
+                        <div className="relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M11.9997 2.75184C18.6567 -4.09116 35.3007 7.88334 11.9997 23.2808C-11.3013 7.88484 5.34267 -4.09116 11.9997 2.75184Z" fill="#F7559E"/>
+                            </svg>
+                            <p className="text-white text-caption absolute top-0 left-[30%]">4</p>
+                        </div>
+                        <p className="text-body text-text1">Click on “Add” to finish</p>
+                    </div>
                 </div>
             </div>
-            <img src={taskImg} alt="a screenshot of adding task modal" />
+            <img className="block" src={taskImg} alt="a screenshot of adding task modal" />
         </section>
     )
 }
