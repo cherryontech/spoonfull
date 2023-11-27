a23250adf798ccd332ddfee7d6adb48f453ca7e9
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-11-27 17:07:25 -0500

    added copy to clipboard functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TasksPage from './pages/TasksPage/TasksPage';
 import FAQ from './pages/FAQ/FAQ';
 import AboutUsPage from './pages/AboutUsPage/AboutUsPage';
 import TutorialPage from './components/TutorialPage/TutorialPage';
-import CopyToClipboard from './pages/CopyToClipboard/CopyToClipboard';
+import ShareSpoonfull from './pages/ShareSpoonfull/ShareSpoonfull';
 import Navbar from './components/Navbar/Navbar';
 import "@fontsource/lato";
 import "@fontsource/lato/700.css";
@@ -44,7 +44,7 @@ function App() {
         <Route path="/tutorial" element={<TutorialPage />}/>
         <Route path="/faq" element={<FAQ />} />
         <Route path="/aboutus" element={<AboutUsPage />} />
-        <Route path="/sharespoonfull" element={<CopyToClipboard />} />
+        <Route path="/sharespoonfull" element={<ShareSpoonfull />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/ShareSpoonfull/ShareSpoonfull.jsx
+++ src/pages/ShareSpoonfull/ShareSpoonfull.jsx
@@ -1,14 +1,24 @@
 import linkImg from "../../assets/link-img.png";
+// import { useState } from "react";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
-const CopyToClipboard = () => {
+const ShareSpoonfull = () => {
+    const link = "https://spoonfull-kiwi-kick.netlify.app/"
+
+    const handleCopy = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <section className="center-column gap-6 h-[80vh]">
             <h4 className="text-header4">Share SpoonFull</h4>
             <p className="text-body text-text1 w-[256px] text-center">Help Your Loved Ones Prioritize Self-Care for a Better Work-Life Balance!</p>
             <img src={linkImg} alt="illustration with two people holding a big link icon" className="w-[214px] md:w-[330px] xl:w-[342px] my-10"/>
-            <button className="btn-filled">Copy Link to Clipboard</button>
+            <CopyToClipboard text={link}>
+                <button className="btn-filled" onClick={handleCopy}>Copy Link to Clipboard</button>
+            </CopyToClipboard>
         </section>
     )
 }
 
-export default CopyToClipboard;
+export default ShareSpoonfull;
