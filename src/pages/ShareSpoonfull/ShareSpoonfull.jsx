19d503b2598ffcc9cb1fdb2957303a2cb0075d55
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-11-28 11:33:37 -0500

    added hover state to the buttons

--- src/pages/ShareSpoonfull/ShareSpoonfull.jsx
+++ src/pages/ShareSpoonfull/ShareSpoonfull.jsx
@@ -1,16 +1,21 @@
 import linkImg from "../../assets/link-img.png";
-// import { useState } from "react";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { ToastContainer, toast } from 'react-toastify';
 
 const ShareSpoonfull = () => {
     const link = "https://spoonfull-kiwi-kick.netlify.app/"
 
     const handleCopy = (e) => {
         e.preventDefault();
+        toast.success("The link was copied to the clipboard!", { theme: "colored", style: { backgroundColor: "#41993F", textAlign: 'center' }, toastId: "successCopy" });
     }
 
     return (
         <section className="center-column gap-6 h-[80vh]">
+            <ToastContainer 
+                position="top-center"
+                autoClose={3000}
+            />
             <h4 className="text-header4">Share SpoonFull</h4>
             <p className="text-body text-text1 w-[256px] text-center">Help Your Loved Ones Prioritize Self-Care for a Better Work-Life Balance!</p>
             <img src={linkImg} alt="illustration with two people holding a big link icon" className="w-[214px] md:w-[330px] xl:w-[342px] my-10"/>
