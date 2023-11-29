d2c99fc68835ff8a6e376fd980e67c917d40b0c1
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-11-29 10:21:56 -0500

    fixed margin clipping on task card

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -44,7 +44,7 @@ const TaskCard = ({ task, onRemoveTask, editChecked, remainingSpoons, handleTask
 
     return (
         <section className="flex justify-between items-center overflow-x-clip relative" onTouchEnd={handleSwipe} onTouchStart={e => setSwipeStart(e.changedTouches[0].clientX)}>
-            <article className="w-full min-w-[328px] py-1 z-[2] bg-background transition ease-in-out">
+            <article className="w-full min-w-[320px] mx-1 py-1 z-[2] bg-background transition ease-in-out">
                 <div className={`bg-${task.background} shadow-card-shadow rounded-lg flex items-start justify-between pl-4`}>
                     <div className="flex py-4 items-center">
                         <input
