94cf6588b0f155d05db644180933ff16edc5d16b
TanyaPina <tanyapina15@gmail.com>
2023-12-04 12:52:24 -0800

    alt text for icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,6 +15,7 @@ import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
 import aboutUsIcon from "../../assets/about-us-icon.svg";
 import faqIcon from "../../assets/faq-icon.svg";
 import shareLinkIcon from "../../assets/share-link-icon.svg";
+import copyrightIcon from "../../assets/copyright-icon.svg";
 
 const Navbar = ({
   taskList,
@@ -48,7 +49,7 @@ const Navbar = ({
     {
       name: 'About Us',
       href: 'aboutus',
-      icon: <img src={aboutUsIcon} />,
+      icon: <img src={aboutUsIcon} alt="about us"/>,
       current: false
     },
     {
@@ -60,12 +61,12 @@ const Navbar = ({
     {
       name: 'FAQ',
       href: 'faq',
-      icon: <img src={faqIcon} />,
+      icon: <img src={faqIcon} alt="faq"/>,
       current: false
     },
     {
       name: 'Share With Friends',
-      icon: <img src={shareLinkIcon} />,
+      icon: <img src={shareLinkIcon} alt="shareable link"/>,
       href: 'sharespoonfull',
       current: false
     },
@@ -212,16 +213,7 @@ const Navbar = ({
           </List>
         </div>
         <footer className="px-4 flex justify-end">
-          <svg className="self-center" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-            <g clip-path="url(#clip0_1455_9560)">
-              <path d="M1 8C1 9.85652 1.7375 11.637 3.05025 12.9497C4.36301 14.2625 6.14348 15 8 15C9.85652 15 11.637 14.2625 12.9497 12.9497C14.2625 11.637 15 9.85652 15 8C15 6.14348 14.2625 4.36301 12.9497 3.05025C11.637 1.7375 9.85652 1 8 1C6.14348 1 4.36301 1.7375 3.05025 3.05025C1.7375 4.36301 1 6.14348 1 8ZM16 8C16 10.1217 15.1571 12.1566 13.6569 13.6569C12.1566 15.1571 10.1217 16 8 16C5.87827 16 3.84344 15.1571 2.34315 13.6569C0.842855 12.1566 0 10.1217 0 8C0 5.87827 0.842855 3.84344 2.34315 2.34315C3.84344 0.842855 5.87827 0 8 0C10.1217 0 12.1566 0.842855 13.6569 2.34315C15.1571 3.84344 16 5.87827 16 8ZM8.146 4.992C6.934 4.992 6.219 5.912 6.219 7.494V8.554C6.219 10.125 6.922 11.016 8.146 11.016C9.125 11.016 9.787 10.43 9.875 9.598H11.17V9.691C11.07 11.139 9.816 12.158 8.14 12.158C6.049 12.158 4.871 10.822 4.871 8.555V7.482C4.871 5.221 6.072 3.844 8.141 3.844C9.822 3.844 11.076 4.898 11.17 6.416V6.504H9.875C9.787 5.625 9.107 4.992 8.146 4.992Z" fill="#3A3A3A" />
-            </g>
-            <defs>
-              <clipPath id="clip0_1455_9560">
-                <rect width="16" height="16" fill="white" />
-              </clipPath>
-            </defs>
-          </svg>
+          <img src={copyrightIcon} alt="copyright symbol"/>
           {mediumScreen ? <p className="pl-2 text-text1 text-body">SpoonFull, all rights reserved</p>
             :
             <p className="pl-2 text-text1 text-caption">SpoonFull, all rights reserved</p>
