764435ecd72ce7f9e912da016749adc3db3a36a4
TanyaPina <tanyapina15@gmail.com>
2023-12-04 11:39:14 -0800

    added responsive styling to drawer

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,8 @@
-import { Fragment } from 'react'
 import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom'
 import { createPortal } from "react-dom";
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { AppBar, Badge, useTheme, styled, ListItem, Divider, List, Drawer, ListItemButton, ListItemText, ListItemIcon } from '@mui/material'
+import { AppBar, Badge, useTheme, useMediaQuery, ListItem, Divider, List, Drawer, ListItemButton, ListItemIcon } from '@mui/material'
 import SpoonsModal from '../SpoonsModal/SpoonsModal';
-import * as React from 'react';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -34,8 +30,6 @@ const Navbar = ({
 
   const [open, setOpen] = useState(false);
 
-  // const pages = ["Home", "About Us", "Tutorial", "FAQ", "Share With Friends"];
-
   const handleDrawerOpen = () => {
     setOpen(true)
   }
@@ -54,7 +48,7 @@ const Navbar = ({
     {
       name: 'About Us',
       href: 'aboutus',
-      icon: <img src={aboutUsIcon}/>,
+      icon: <img src={aboutUsIcon} />,
       current: false
     },
     {
@@ -66,21 +60,16 @@ const Navbar = ({
     {
       name: 'FAQ',
       href: 'faq',
-      icon: <img src={faqIcon}/>,
+      icon: <img src={faqIcon} />,
       current: false
     },
     {
       name: 'Share With Friends',
-      icon: <img src={shareLinkIcon}/>,
+      icon: <img src={shareLinkIcon} />,
       href: 'sharespoonfull',
       current: false
     },
   ]
-
-  // function classNames(...classes) {
-  //   ``
-  //   return classes.filter(Boolean).join(' ')
-  // }
 
   useEffect(() => {
     const spoonCount = (arr) => {
@@ -108,15 +97,9 @@ const Navbar = ({
 
   }, [taskList])
 
-  const DrawerHeader = styled('div')((({ theme }) => ({
-    display: "flex",
-    alignItems: "center",
-    padding: theme.padding(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end'
-  })))
-
-  const drawerWidth = 241
+  const theme = useTheme();
+  const largeScreen = useMediaQuery(theme.breakpoints.up("md"))
+  const mediumScreen = useMediaQuery(theme.breakpoints.up("sm"))
 
   return (
     <div>
@@ -170,14 +153,39 @@ const Navbar = ({
         </AppBar>
       </Box>
       <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box'
-          },
-        }}
+        PaperProps={
+          largeScreen ? {
+            sx: {
+              backgroundColor: '#E8D7DF',
+              width: 440,
+              pb: 2,
+              pt: 3,
+              px: 3,
+            }
+          }
+            :
+            mediumScreen ? {
+              sx: {
+                backgroundColor: '#E8D7DF',
+                width: 360,
+                pb: 2,
+                pt: 3,
+                px: 2,
+              }
+            }
+              :
+              {
+                sx: {
+                  backgroundColor: '#E8D7DF',
+                  width: 280,
+                  pb: 2,
+                  pt: 3,
+                  pl: 1,
+                }
+              }
+        }
+        sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}
+        style={{ bgcolor: "accent3" }}
         variant="temporary"
         anchor="left"
         onClose={() => {
@@ -185,21 +193,40 @@ const Navbar = ({
         }}
         open={open}
       >
-        <Divider />
-        <List>
-          {navigation.map((item) => {
-            return (
-              <ListItem>
-                <ListItemButton
-                  key={item.name}
-                  to={item.href}>
-                    <ListItemIcon style={{ color: "#001111" }}> {item.icon} </ListItemIcon>
-                  {item.name}
-                </ListItemButton>
-              </ListItem>
-            )
-          })}
-        </List>
+        <div className="felx flex-col flex-1 items-start">
+          <h4 className="text-header4 p-2 indent-4">Menu</h4>
+          <Divider variant="middle" />
+          <List >
+            {navigation.map((item) => {
+              return (
+                <ListItem>
+                  <ListItemButton
+                    key={item.name}
+                    to={item.href}>
+                    <ListItemIcon sx={{ mr: -3 }} style={{ color: "#001111" }}> {item.icon} </ListItemIcon>
+                    {item.name}
+                  </ListItemButton>
+                </ListItem>
+              )
+            })}
+          </List>
+        </div>
+        <footer className="px-4 flex justify-end">
+          <svg className="self-center" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <g clip-path="url(#clip0_1455_9560)">
+              <path d="M1 8C1 9.85652 1.7375 11.637 3.05025 12.9497C4.36301 14.2625 6.14348 15 8 15C9.85652 15 11.637 14.2625 12.9497 12.9497C14.2625 11.637 15 9.85652 15 8C15 6.14348 14.2625 4.36301 12.9497 3.05025C11.637 1.7375 9.85652 1 8 1C6.14348 1 4.36301 1.7375 3.05025 3.05025C1.7375 4.36301 1 6.14348 1 8ZM16 8C16 10.1217 15.1571 12.1566 13.6569 13.6569C12.1566 15.1571 10.1217 16 8 16C5.87827 16 3.84344 15.1571 2.34315 13.6569C0.842855 12.1566 0 10.1217 0 8C0 5.87827 0.842855 3.84344 2.34315 2.34315C3.84344 0.842855 5.87827 0 8 0C10.1217 0 12.1566 0.842855 13.6569 2.34315C15.1571 3.84344 16 5.87827 16 8ZM8.146 4.992C6.934 4.992 6.219 5.912 6.219 7.494V8.554C6.219 10.125 6.922 11.016 8.146 11.016C9.125 11.016 9.787 10.43 9.875 9.598H11.17V9.691C11.07 11.139 9.816 12.158 8.14 12.158C6.049 12.158 4.871 10.822 4.871 8.555V7.482C4.871 5.221 6.072 3.844 8.141 3.844C9.822 3.844 11.076 4.898 11.17 6.416V6.504H9.875C9.787 5.625 9.107 4.992 8.146 4.992Z" fill="#3A3A3A" />
+            </g>
+            <defs>
+              <clipPath id="clip0_1455_9560">
+                <rect width="16" height="16" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          {mediumScreen ? <p className="pl-2 text-text1 text-body">SpoonFull, all rights reserved</p>
+            :
+            <p className="pl-2 text-text1 text-caption">SpoonFull, all rights reserved</p>
+          }
+        </footer>
       </Drawer>
     </div>
   );
