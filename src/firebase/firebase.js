e056d4a76159be47ab9ae4b32607d2772a237f6d
Jiachun Xiang <jcxiang1230@gmail.com>
2024-06-29 18:11:33 -0400

    add app for export

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,4 +15,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
-export { auth, googleProvider };
+export { app, auth, googleProvider };
